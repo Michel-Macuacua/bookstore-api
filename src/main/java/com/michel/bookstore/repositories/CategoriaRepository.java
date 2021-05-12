6cece34f81d4dbe535f418421d26bf3ce7fa48c9
michel-macuacua <michelfernandes802@gmail.com>
2021-05-12 19:47:58 +0200

    Repositories implementados

---
+++ src/main/java/com/michel/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.michel.bookstore.repositories;
+
+import com.michel.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
---
+++ src/main/java/com/michel/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,8 @@
+package com.michel.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
