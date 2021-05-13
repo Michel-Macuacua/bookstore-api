4abe3bda84aff63b07fb8301e04d1fb9d84822d9
michel-macuacua <michelfernandes802@gmail.com>
2021-05-13 13:58:23 +0200

    Perfil de Teste implementado

--- src/main/java/com/michel/bookstore/BookstoreApplication.java
+++ src/main/java/com/michel/bookstore/BookstoreApplication.java
@@ -1,42 +1,15 @@
 package com.michel.bookstore;
 
-import com.michel.bookstore.domain.Categoria;
-import com.michel.bookstore.domain.Livro;
-import com.michel.bookstore.repositories.CategoriaRepository;
-import com.michel.bookstore.repositories.LivroRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-
-	@Autowired
-	private LivroRepository livroRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception{
-		Categoria cat1 = new Categoria(null, "Informatica", "Livros de TI");
-
-		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
-
-		cat1.getLivros().addAll(Arrays.asList(l1));
-
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
+public class BookstoreApplication {
 
 
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
 
 }
---
+++ src/main/java/com/michel/bookstore/config/TestConfig.java
@@ -0,0 +1,20 @@
+package com.michel.bookstore.config;
+
+import com.michel.bookstore.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void instaciaBaseDeDados(){
+        this.dbService.instanciaBaseDeDados();
+    }
+}
