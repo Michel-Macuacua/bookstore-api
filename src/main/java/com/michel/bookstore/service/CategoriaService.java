ba431f387edc239aa7c411ad04daebd1e6de32cf
michel-macuacua <michelfernandes802@gmail.com>
2021-05-14 18:36:50 +0200

    Excessao personalizada para objecto nao encontrado

--- src/main/java/com/michel/bookstore/service/CategoriaService.java
+++ src/main/java/com/michel/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.michel.bookstore.domain.Categoria;
 import com.michel.bookstore.repositories.CategoriaRepository;
+import com.michel.bookstore.service.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,14 +16,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class CategoriaService {
 
-
     @Autowired
     private CategoriaRepository repository;
 
-    public Categoria findById(Integer id){
+    public Categoria findById(Integer id) {
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objecto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
-    
 }
