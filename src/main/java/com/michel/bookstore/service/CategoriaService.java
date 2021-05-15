a870ae853e6a673d2251c67a029cae505c935a2b
michel-macuacua <michelfernandes802@gmail.com>
2021-05-15 11:00:18 +0200

    Delete para Categoria

--- src/main/java/com/michel/bookstore/service/CategoriaService.java
+++ src/main/java/com/michel/bookstore/service/CategoriaService.java
@@ -43,5 +43,10 @@ public class CategoriaService {
         return repository.save(obj);
     }
 
+    public void delete (Integer id){
+        findById(id);
+        repository.deleteById(id);
+    }
+
 
 }
