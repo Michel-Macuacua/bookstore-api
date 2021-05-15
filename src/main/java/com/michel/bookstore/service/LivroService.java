9f723253b4d356f0a7b5d38af38f4acb9ea17bce
michel-macuacua <michelfernandes802@gmail.com>
2021-05-15 19:48:15 +0200

    DELETE para Livro

--- src/main/java/com/michel/bookstore/service/LivroService.java
+++ src/main/java/com/michel/bookstore/service/LivroService.java
@@ -49,4 +49,9 @@ public class LivroService {
         return repository.save(obj);
     }
 
+    public void delete(Integer id){
+        Livro obj = findById(id);
+        repository.delete(obj);
+    }
+
 }
