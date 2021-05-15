22e0f3fd3ca5d3014845bb483f84a48683d6c064
michel-macuacua <michelfernandes802@gmail.com>
2021-05-15 14:06:51 +0200

    DataIntegrityViolationException implementado para Categoria

--- src/main/java/com/michel/bookstore/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/michel/bookstore/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.michel.bookstore.resources.exceptions;
 
 import javax.servlet.ServletRequest;
 
+import com.michel.bookstore.service.exceptions.DataIntegrityViolationException;
 import com.michel.bookstore.service.exceptions.ObjectNotFoundException;
 
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,12 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e, ServletRequest request){
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
     
 }
--- src/main/java/com/michel/bookstore/service/CategoriaService.java
+++ src/main/java/com/michel/bookstore/service/CategoriaService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import com.michel.bookstore.domain.Categoria;
 import com.michel.bookstore.dtos.CategoriaDTO;
 import com.michel.bookstore.repositories.CategoriaRepository;
+import com.michel.bookstore.service.exceptions.DataIntegrityViolationException;
 import com.michel.bookstore.service.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,26 +28,31 @@ public class CategoriaService {
                 "Objecto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
-    public List<Categoria> findAll(){
+    public List<Categoria> findAll() {
         return repository.findAll();
     }
 
-    public Categoria create(Categoria obj){
+    public Categoria create(Categoria obj) {
         obj.setId(null);
         return repository.save(obj);
     }
 
-    public Categoria update (Integer id, CategoriaDTO objDto){
+    public Categoria update(Integer id, CategoriaDTO objDto) {
         Categoria obj = findById(id);
         obj.setNome(objDto.getNome());
         obj.setDescricao(objDto.getDescricao());
         return repository.save(obj);
     }
 
-    public void delete (Integer id){
+    public void delete(Integer id) {
         findById(id);
-        repository.deleteById(id);
-    }
+        try {
+            repository.deleteById(id);
+        } catch (org.springframework.dao.DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException(
+                    "Categoria nao pode ser deletada! Possui livros associados");
+        }
 
+    }
 
 }
