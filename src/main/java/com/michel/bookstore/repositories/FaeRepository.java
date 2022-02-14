a0c884d0cec82296c79eb4876b4469af48bafadd
Michel-Macuacua <82373775+Michel-Macuacua@users.noreply.github.com>
2022-02-14 10:51:11 +0200

    Add files via upload

---
+++ src/main/java/com/michel/bookstore/repositories/FaeRepository.java
@@ -0,0 +1,10 @@
+package mz.gov.cedsif.datavalidapi.repositories;
+
+import mz.gov.cedsif.datavalidapi.domain.Fae;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FaeRepository extends JpaRepository<Fae, Integer>
+{
+}
