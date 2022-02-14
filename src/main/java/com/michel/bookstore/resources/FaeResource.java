fccf868a0598b0642e6a745bfab57cf44fd70658
Michel-Macuacua <82373775+Michel-Macuacua@users.noreply.github.com>
2022-02-14 10:52:05 +0200

    Add files via upload

---
+++ src/main/java/com/michel/bookstore/resources/FaeResource.java
@@ -0,0 +1,75 @@
+package mz.gov.cedsif.datavalidapi.resources;
+
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import mz.gov.cedsif.datavalidapi.domain.Fae;
+import mz.gov.cedsif.datavalidapi.domain.Fae;
+//import mz.gov.cedsif.datavalidapi.dtos.FaeDTO;
+import mz.gov.cedsif.datavalidapi.service.FaeService;
+import mz.gov.cedsif.datavalidapi.service.FaeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping(value = "/faes")
+public class FaeResource {
+
+    @Autowired
+    private FaeService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Fae> findById(@PathVariable Integer id) {
+        Fae obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Fae>> findAll() {
+        List<Fae> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+//    @PutMapping(value = "/{id}")
+//    public ResponseEntity<Fae> update(@PathVariable Integer id, @Valid @RequestBody Fae obj) {
+//        Fae newObj = service.update(id, obj);
+//        return ResponseEntity.ok().body(newObj);
+//    }
+//
+//    @PatchMapping(value = "/{id}")
+//    public ResponseEntity<Fae> updatePatch(@PathVariable Integer id, @Valid @RequestBody Fae obj) {
+//        Fae newObj = service.update(id, obj);
+//        return ResponseEntity.ok().body(newObj);
+//    }
+
+    @PostMapping
+    public ResponseEntity<Fae> create(@Valid @RequestBody Fae obj) {
+        Fae newObj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/Faes/{id}")
+                .buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
