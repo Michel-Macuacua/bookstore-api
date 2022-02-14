e4c605c0c63588d240578ea941c88216256c601a
Michel-Macuacua <82373775+Michel-Macuacua@users.noreply.github.com>
2022-02-14 10:47:50 +0200

    Add files via upload

---
+++ src/main/java/com/michel/bookstore/domain/Fae.java
@@ -0,0 +1,70 @@
+package mz.gov.cedsif.datavalidapi.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Fae {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer nuit;
+    private String name;
+    private String dateOfBirth;
+
+    public Fae() {
+    }
+
+    public Fae(Integer id, Integer nuit, String name, String dateOfBirth) {
+        this.id = id;
+        this.nuit = nuit;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getNuit() {
+        return nuit;
+    }
+
+    public void setNuit(int nuit) {
+        this.nuit = nuit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Fae{" +
+                "id=" + id +
+                ", nuit=" + nuit +
+                ", name='" + name + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
+}
