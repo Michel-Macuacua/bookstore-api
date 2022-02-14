446da6b97e7dd43950c864187332a99b51e0bf61
Michel-Macuacua <82373775+Michel-Macuacua@users.noreply.github.com>
2022-02-14 10:51:38 +0200

    Update FaeRepository.java

--- src/main/java/com/michel/bookstore/repositories/FaeRepository.java
+++ src/main/java/com/michel/bookstore/repositories/FaeRepository.java
@@ -1,4 +1,4 @@
-package mz.gov.cedsif.datavalidapi.repositories;
+package com.michel.bookstore.domain;
 
 import mz.gov.cedsif.datavalidapi.domain.Fae;
 import org.springframework.data.jpa.repository.JpaRepository;
